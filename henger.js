cbf58ad2b5ae34527b00ddec85fbcd3ecdb2cb52
Your Name <meri.levente@students.jedlik.eu>
2021-05-16 16:49:14 +0200

    +

--- henger.js
+++ henger.js
@@ -12,7 +12,7 @@ function henger() {
     alert("A magasság nem lehet 0 vagy negatív szám!");
     return;
   }
-  var henger_a = 2 * sugar * Math.PI * (sugar * magassag);
+  var henger_a = 2 * sugar * Math.PI * (sugar + magassag);
   var henger_v = sugar * sugar * Math.PI * magassag;
   document.getElementById("henger_a").value = henger_a.toLocaleString();
   document.getElementById("henger_v").value = henger_v.toLocaleString();
